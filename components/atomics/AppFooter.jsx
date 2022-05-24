7c32c6b241859ef6a579497ac7e44743c79132fc
jdel54 <101069613+jdel54@users.noreply.github.com>
2022-05-24 19:41:25 +0200

    3rd commit

--- components/atomics/AppFooter.jsx
+++ components/atomics/AppFooter.jsx
@@ -1,8 +1,6 @@
 import USER from '../../data/user.json';
 
 const AppFooter = () => {
-
-
   return (
     <footer className="text-center text-xs md:text-md text-gray py-7">
       <p className="mb-1">❤️ {USER.name}</p>  
