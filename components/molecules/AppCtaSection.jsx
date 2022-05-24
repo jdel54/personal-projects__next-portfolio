364d01e2874d6616714957691090ea0e01ff51de
jdel54 <101069613+jdel54@users.noreply.github.com>
2022-05-24 19:38:46 +0200

    2nd commit

--- components/molecules/AppCtaSection.jsx
+++ components/molecules/AppCtaSection.jsx
@@ -4,7 +4,7 @@ const AppCtaSection = () => (
   <section className="py-20 border-b border-light-gray">
     <div className="text-center">
       <h2 className="text-4xl lg:text-5xl font-bold text-black">
-        Don't be shy
+        {'Don\'t be shy'}
       </h2>
       <p className="text-xs md:text-sm text-gray max-w-[260px] md:max-w-xs mx-auto mt-2">
         Is there anything I can help you with?  Thank you for visiting.
